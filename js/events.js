3498faf1f0ae594e3fd36e87bec8702455095570
Souji 2613 <puppypoola@gmail.com>
2018-06-13 21:59:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -22,3 +22,12 @@ function pressIt(){
   })
 }
 
+function submitIt(){
+  $("form").on("submit", function(){
+    if($("input: first").val() === "correct"){
+      alert('your form is going to be submitted now');
+      return;
+    }
+  })
+}
+
